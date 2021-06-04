999cf34d794c378fae6a3cb9f20f576dca7df7ca
Bastiaan Hoeben <76174738+bastiaanhoeben@users.noreply.github.com>
2021-06-04 11:12:07 +0200

    Rename database tables

--- sql_queries.py
+++ sql_queries.py
@@ -1,32 +1,32 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP table songplay_table"
-user_table_drop = "DROP table user_table"
-song_table_drop = "DROP table song_table"
-artist_table_drop = "DROP table artist_table"
-time_table_drop = "DROP table time_table"
+songplay_table_drop = "DROP table songplays"
+user_table_drop = "DROP table users"
+song_table_drop = "DROP table songs"
+artist_table_drop = "DROP table artists"
+time_table_drop = "DROP table time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplay_table (
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (
 songplay_id SERIAL PRIMARY KEY, start_time TIMESTAMP, user_id INT, 
 level VARCHAR, song_id VARCHAR, artist_id VARCHAR, session_id INT, location 
 VARCHAR, user_agent VARCHAR)""")
 
-user_table_create = ("""CREATE TABLE IF NOT EXISTS user_table (user_id INT 
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id INT 
 PRIMARY KEY, first_name VARCHAR, last_name VARCHAR, gender VARCHAR(1), 
 level VARCHAR)""")
 
-song_table_create = ("""CREATE TABLE IF NOT EXISTS song_table (song_id 
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id 
 VARCHAR PRIMARY KEY, title VARCHAR, artist_id VARCHAR, year INT, duration 
 REAL""")
 
-artist_table_create = ("""CREATE TABLE IF NOT EXISTS artist_table
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists
 (artist_id VARCHAR PRIMARY KEY, name VARCHAR, location VARCHAR, latitude REAL, 
 longitude REAL
 """)
 
-time_table_create = ("""CREATE TABLE IF NOT EXISTS time_table
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time
 (start_time TIMESTAMP, hour INT, day VARCHAR, week INT, month INT, year INT, 
 weekday VARCHAR
 """)
